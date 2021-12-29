8088577ba32340a9815349cad05eb85bd55407c3
Илья <kovalchukilya5@gmail.com>
2021-12-29 13:02:16 +0300

    Add CLI options.

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ load_dotenv()
 
 CHAT_HOST = os.getenv('CHAT_HOST', 'host')
 CHAT_PORT = os.getenv('CHAT_PORT', 'port')
+HISTORY_PATH = os.getenv('HISTORY_PATH', 'test.txt')
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import asyncio
-from config import CHAT_HOST, CHAT_PORT
+from config import CHAT_HOST, CHAT_PORT, HISTORY_PATH
 import datetime
 import aiofiles
+import argparse
 
 
-async def tcp_echo_client(host, port):
+async def tcp_echo_client(host, port, filepath):
     print(host)
     reader, writer = await asyncio.open_connection(
         host, port)
@@ -15,7 +16,7 @@ async def tcp_echo_client(host, port):
             formatted_date = now.strftime("%d.%m.%Y %H:%M")
             message = f'[{formatted_date}] {data.decode()!r}'
             print(message)
-            async with aiofiles.open('test.txt', mode='a') as f:
+            async with aiofiles.open(filepath, mode='a') as f:
                 await f.write(message)
     finally:
         print('Close the connection')
@@ -23,4 +24,17 @@ async def tcp_echo_client(host, port):
 
 
 if __name__ == '__main__':
-    asyncio.run(tcp_echo_client(CHAT_HOST, CHAT_PORT))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host', type=str, help='Set host')
+    parser.add_argument('--port', type=int, help='Set port')
+    parser.add_argument(
+        '--history',
+        type=str,
+        help='Set history path',
+    )
+    args = parser.parse_args()
+
+    chat_host =  args.host or CHAT_HOST
+    chat_port = args.port or CHAT_PORT
+    history_path = args.history or HISTORY_PATH
+    asyncio.run(tcp_echo_client(chat_host, chat_port, history_path))
