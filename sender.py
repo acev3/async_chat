113dd97223b747463cdbbd0ee28ededcd3b57bef
Илья <kovalchukilya5@gmail.com>
2021-12-30 12:20:22 +0300

    Add check for broken token.

--- sender.py
+++ sender.py
@@ -1,29 +1,33 @@
 import asyncio
 from config import CHAT_HOST, SENDER_PORT, ACCOUNT_HASH
 import logging
+import json
 
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger('SENDER')
 
 async def tcp_echo_client(chat_host, chat_port, message):
+    ACCOUNT_HASH = '123'
     reader, writer = await asyncio.open_connection(
             chat_host, chat_port)
+    response = await reader.readline()
+    logger.debug(response)
+    logger.debug(f'Send: token')
+    token = "{}\n\n".format(ACCOUNT_HASH)
+    writer.write(token.encode("utf-8"))
+    await writer.drain()
+    response = await reader.readline()
+    logger.debug(response)
+    check_token = response.decode('utf8').replace("'", '"')
+    response = await reader.readline()
+    logger.debug(response)
     while True:
-        response = await reader.readline()
-        logger.debug(response)
-        logger.debug(f'Send: token')
-        token = "{}\n".format(ACCOUNT_HASH)
-        writer.write(token.encode("utf-8"))
-        await writer.drain()
-        response = await reader.readline()
-        logger .debug(response)
-        await asyncio.sleep(1)
-        response = await reader.readline()
-        logger.debug(response)
-        message = "{}\n".format(message)
+        if json.loads(check_token) is None:
+            logger.debug('Неизвестный токен. Проверьте его или зарегистрируйте заново.')
+            break
+        message = "{}\n\n".format(message)
         logger.debug(f'Send: {message}')
-        await asyncio.sleep(1)
         writer.write(message.encode('utf-8'))
         await writer.drain()
     logger .debug('Close the connection')
